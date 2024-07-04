e8af7a9a80867226976050d25730a67e8684d2f8
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-04 20:11:15 -0300

    fix: ensure cpfCodes and cnpjCodes return only digits

--- PGFN/utils/helpers/separate-id-codes.js
+++ PGFN/utils/helpers/separate-id-codes.js
@@ -2,8 +2,12 @@ const ClearCPF = require("../cpf/clear-cpf-chars");
 const ClearCNPJ = require("../cnpj/clear-cnpj-chars");
 
 const separateIdCodes = (idCodes) => {
-  const cpfCodes = idCodes.filter((idCode) => ClearCPF(idCode).length === 11);
-  const cnpjCodes = idCodes.filter((idCode) => ClearCNPJ(idCode).length === 14);
+  const cpfCodes = idCodes
+    .filter((idCode) => ClearCPF(idCode).length === 11)
+    .map((idCode) => ClearCPF(idCode));
+  const cnpjCodes = idCodes
+    .filter((idCode) => ClearCNPJ(idCode).length === 14)
+    .map((idCode) => ClearCNPJ(idCode));
   const anotherCodes = idCodes.filter(
     (idCode) =>
       ClearCNPJ(idCode).length !== 14 && ClearCPF(idCode).length !== 11
