a1be2ca54c5d7183cd7654051bdb0ca132f9d9c6
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-04 11:35:27 -0300

    feat: ensure another idCodes are returned

--- PGFN/utils/helpers/separate-id-codes.js
+++ PGFN/utils/helpers/separate-id-codes.js
@@ -3,10 +3,13 @@ const ClearCNPJ = require("../cnpj/clear-cnpj-chars");
 
 const separateIdCodes = (idCodes) => {
   const cpfCodes = idCodes.filter((idCode) => ClearCPF(idCode).length === 11);
-
   const cnpjCodes = idCodes.filter((idCode) => ClearCNPJ(idCode).length === 14);
+  const anotherCodes = idCodes.filter(
+    (idCode) =>
+      ClearCNPJ(idCode).length !== 14 && ClearCPF(idCode).length !== 11
+  );
 
-  return { cpf: cpfCodes, cnpj: cnpjCodes };
+  return { cpf: cpfCodes, cnpj: cnpjCodes, another: anotherCodes };
 };
 
 module.exports = separateIdCodes;
