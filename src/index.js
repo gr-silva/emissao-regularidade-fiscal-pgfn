762dddaccb176567313a3dbec601e1086478d75c
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-03 23:16:32 -0300

    feat: add initial commit of main process

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const PGFN = require("../PGFN/src");
+const separateIdCodes = require("../PGFN/utils/helpers/separate-id-codes.js");
+const mockIdCodes = require("./mock.js");
 
+const separatedIdCodes = separateIdCodes(mockIdCodes);
+
+const pgfn = new PGFN(separatedIdCodes.cpf, "PF");
+pgfn.generateTaxRegularityCertificate();
