2a7d719149e6d70f0662d61df6905c8c46f543c9
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-03 23:01:21 -0300

    feat: add processmentType and initial generateTaxRegularityCertificate function

--- PGFN/src/index.js
+++ PGFN/src/index.js
@@ -1,29 +1,45 @@
 const WebRobot = require("../WebRobot/src/WebRobot");
 const selectors = require("../configs/selectors");
 const configs = require("../configs/general");
-const ValidateCPF = require("../utils/validate-cpf");
-const ValidateCNPJ = require("../utils/validate-cnpj");
 
 class PGFN {
-  constructor(idCode, timeout = 30000) {
+  constructor(idCodes = [], processmentType = "", timeout = 30000) {
     this._timeout = timeout;
     this._browser = "";
     this._page = "";
     this._robot = new WebRobot(timeout);
     this._selectors = selectors;
-    this._idCode = idCode;
+    this._idCodes = idCodes;
+    this._processmentType = processmentType;
+
+    if (this._idCodes.length === 0) {
+      throw new Error("Nenhuma lista com CPF ou CNPJ foi informado.");
+    }
+
+    if (this._processmentType !== "PF" && this._processmentType !== "PJ") {
+      throw new Error(
+        "Tipo de processamento inválido. Tipos aceitáveis: PF ou PJ."
+      );
+    }
   }
 
   async start(LIMITER = 3) {
-    //TODO: INCLUIR VERIFICAÇÃO SE É CPF OU CNPJ. SE FOR CPF, ACESSAR PF_URL, SE FOR CNPJ, ACESSAR PJ_URL
-    if (ValidateCPF(this._idCode)) {
+    //TODO: IMPLEMENTAR VERIFICACAO DE ERROS E TENTATIVAS
+    if (this._processmentType === "PF") {
       this._robot.start(configs.PF_URL, configs.BROWSER_OPTIONS);
     }
-    if (ValidateCNPJ(this._idCode)) {
+    if (this._processmentType === "PJ") {
       this._robot.start(configs.PJ_URL, configs.BROWSER_OPTIONS);
     }
 
     return "CPF ou CNPJ Inválido.";
+  }
+
+  async generateTaxRegularityCertificate() {
+    // await this.start();
+    for (const idCode of this._idCodes) {
+      console.log(idCode);
+    }
   }
 }
 
