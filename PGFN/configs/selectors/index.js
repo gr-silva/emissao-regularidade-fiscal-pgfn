fab6906a69906d9c256b74b7adcce6a70182e277
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-04 11:58:01 -0300

    feat: add initial selectors of process

---
+++ PGFN/configs/selectors/index.js
@@ -0,0 +1,19 @@
+const INPUTS = {
+  ID_CODE: "#NI",
+};
+
+const BUTTONS = {
+  CONSULT: "#validar",
+  ISSUE_NEW_CERTIFICATE: "#FrmSelecao > a:nth-child(6)",
+  NEW_CONSULT:
+    "#main-container > div > table > tbody > tr > td > div > a:nth-child(1) > input[type=button]",
+};
+
+const MODALS = {
+  ERROR:
+    "body > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons.ui-draggable",
+};
+
+const MESSAGES = {
+  ERROR_MESSAGE: "#mensagem",
+};
