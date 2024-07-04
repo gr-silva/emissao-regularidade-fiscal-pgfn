34b90429e6ddd19d7cffb0ea1a3537034114499f
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-04 11:58:22 -0300

    fix: ensure mock.js path are correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const PGFN = require("../PGFN/src");
 const separateIdCodes = require("../PGFN/utils/helpers/separate-id-codes.js");
-const mockIdCodes = require("./mock.js");
+const mockIdCodes = require("../mock.js");
 
 const separatedIdCodes = separateIdCodes(mockIdCodes);
 
