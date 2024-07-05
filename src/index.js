3e734d0b18749c4f3542df8231195ed992ee5fad
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-05 00:00:33 -0300

    docs: add to-do task comment

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ const mockIdCodes = require("../mock.js");
 
 const separatedIdCodes = separateIdCodes(mockIdCodes);
 
+//TODO: CRIAR PARALELISMO COM puppeteer-cluster E INSERIR ETAPA DE CNPJ NO FLUXO ABAIXO.
 const pgfn = new PGFN(separatedIdCodes.cpf, "PF");
 pgfn.generateTaxRegularityCertificate();
