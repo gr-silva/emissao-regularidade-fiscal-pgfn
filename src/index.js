9f624e0ff5c7acbc992e3992afc64e1ad44dbd6a
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-06 12:09:57 -0300

    feat: add api server and parallel processment

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const PGFN = require("../PGFN/src");
-const separateIdCodes = require("../PGFN/utils/helpers/separate-id-codes.js");
-const mockIdCodes = require("../mock.js");
-
-const separatedIdCodes = separateIdCodes(mockIdCodes);
-
-//TODO: CRIAR PARALELISMO COM puppeteer-cluster E INSERIR ETAPA DE CNPJ NO FLUXO ABAIXO.
-const pgfn = new PGFN(separatedIdCodes.cpf, "PF");
-pgfn.generateTaxRegularityCertificate();
---
+++ src/server.js
@@ -0,0 +1,41 @@
+const PGFN = require("../PGFN/src/index.js");
+const ValidateCNPJ = require("../PGFN/utils/cnpj/validate-cnpj.js");
+const ValidateCPF = require("../PGFN/utils/cpf/validate-cpf.js");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/gerar-certidoes", async (req, res) => {
+  const { documentNumbers } = req.body;
+  if (!Array.isArray(documentNumbers)) {
+    return res
+      .status(400)
+      .json({ error: "O input deve ser um array de CPFs ou CNPjs." });
+  }
+  const results = await Promise.all(
+    documentNumbers.map(async (document) => {
+      if (ValidateCPF(document)) {
+        const pgfn = new PGFN([document], "PF");
+        return pgfn.generateTaxRegularityCertificate();
+      } else if (ValidateCNPJ(document)) {
+        const pgfn = new PGFN([document], "PJ");
+        return pgfn.generateTaxRegularityCertificate();
+      } else {
+        return {
+          [document]: {
+            status: "falha",
+            certidao: null,
+            motivo_erro: "CPF ou CNPJ inválido.",
+          },
+        };
+      }
+    })
+  );
+  res.json(results);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
