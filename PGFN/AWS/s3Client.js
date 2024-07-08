e7ae27f1e8be33745b3dec7aa541cb6078f8bb9d
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-08 16:08:25 -0300

    fix: update apiVersion

--- PGFN/AWS/s3Client.js
+++ PGFN/AWS/s3Client.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 async function uploadFile(fileName, filePath, mimeType) {
   const s3 = new AWS.S3({
-    apiVersion: "2006-03-01",
+    apiVersion: "2012-10-17",
     region: process.env.AWS_REGION,
   });
   const fileContent = fs.readFileSync(filePath);
@@ -20,7 +20,7 @@ async function uploadFile(fileName, filePath, mimeType) {
 
 async function listObjects(filter) {
   const s3 = new AWS.S3({
-    apiVersion: "2006-03-01",
+    apiVersion: "2012-10-17",
     region: process.env.AWS_REGION,
   });
   const params = {
