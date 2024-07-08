ca9169cfe00a208645c3807198cf847d4d0e31bc
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-07-08 11:52:19 -0300

    feat: add chunk array of cpf and cnpj

--- src/routes/generate-certificates.js
+++ src/routes/generate-certificates.js
@@ -3,6 +3,7 @@ const ValidateCNPJ = require("../../PGFN/utils/cnpj/validate-cnpj.js");
 const ClearCNPJ = require("../../PGFN/utils/cnpj/clear-cnpj-chars.js");
 const ClearCPF = require("../../PGFN/utils/cpf/clear-cpf-chars.js");
 const ValidateCPF = require("../../PGFN/utils/cpf/validate-cpf.js");
+const chunkArray = require("../../PGFN/utils/helpers/create-chunk-array");
 
 module.exports = (app) => {
   app.post("/gerar-certidoes", async (req, res) => {
@@ -12,25 +13,34 @@ module.exports = (app) => {
         .status(400)
         .json({ error: "O input deve ser um array de CPFs ou CNPjs." });
     }
-    const results = await Promise.all(
-      taxPayersId.map(async (taxPayerId) => {
-        if (ValidateCPF(taxPayerId)) {
-          const pgfn = new PGFN([ClearCPF(taxPayerId)], "PF");
-          return pgfn.generateTaxRegularityCertificate();
-        } else if (ValidateCNPJ(taxPayerId)) {
-          const pgfn = new PGFN([ClearCNPJ(taxPayerId)], "PJ");
-          return pgfn.generateTaxRegularityCertificate();
-        } else {
-          return {
-            [taxPayerId]: {
-              status: "falha",
-              certidao: null,
-              motivo_erro: "CPF ou CNPJ inválido.",
-            },
-          };
-        }
-      })
-    );
+    const chunkTaxPayersId = chunkArray(taxPayersId, 4);
+    const results = {};
+
+    for (const chunkedTaxPayerId of chunkTaxPayersId) {
+      const chunkResults = await Promise.all(
+        chunkedTaxPayerId.map(async (taxPayerId) => {
+          if (ValidateCPF(taxPayerId)) {
+            const pgfn = new PGFN([ClearCPF(taxPayerId)], "PF");
+            return pgfn.generateTaxRegularityCertificate();
+          } else if (ValidateCNPJ(taxPayerId)) {
+            const pgfn = new PGFN([ClearCNPJ(taxPayerId)], "PJ");
+            return pgfn.generateTaxRegularityCertificate();
+          } else {
+            return {
+              [taxPayerId]: {
+                status: "falha",
+                certidao: null,
+                motivo_erro: "CPF ou CNPJ inválido.",
+              },
+            };
+          }
+        })
+      );
+      chunkResults.forEach((result) => {
+        Object.assign(results, result);
+      });
+    }
+
     res.json(results);
   });
 };
